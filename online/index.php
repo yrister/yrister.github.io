ec7a9e821ce6a36fb30d574bb1cf6d4ada0612f2
Roman <yrister@mail.ru>
2016-02-29 20:53:26 +0300

    Update index.php

--- online/index.php
+++ online/index.php
@@ -1,10 +1,4 @@
 <?php
 
   echo "good";
-  $name1 = fopen("text1.txt", "a");
-    //"a" -говорит о ДОзаписи
-    //"w" - о переписывании всего
-    //"r" -для чтения
-  fputs ($name1, "hello "."\n");
-  fclose ($name1);
 ?>
